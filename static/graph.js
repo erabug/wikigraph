82ddb858eb8e1c2aee5a6f9c313fb0bb1a94c209
erabug <erabug@gmail.com>
2014-11-13 11:11:08 -0800

    cleaned up graph.js file

--- static/graph.js
+++ static/graph.js
@@ -14,82 +14,64 @@ function drawGraph(json) {
       .charge(-100)
       .size([width, height]);
 
-  // d3.json('response.json', function(error, json) {
-    force
-        .nodes(json.nodes)
-        .links(json.links)
-        .start();
+  force
+      .nodes(json.nodes)
+      .links(json.links)
+      .start();
 
-    svg.append("svg:defs").selectAll("marker")
-        .data(["arrow"])
-      .enter().append("svg:marker")
-        .attr("id", String)
-        .attr("viewBox", "0 -5 10 10")
-        .attr("refX", 23)
-        // .attr("refY", 0)
-        .attr("markerWidth", 6)
-        .attr("markerHeight", 6)
-        .attr("orient", "auto")
-        .append("svg:path")
-        .attr("d", "M0,-4L10,0L0,4Z");
+  svg.append("svg:defs").selectAll("marker")
+      .data(["arrow"])
+    .enter().append("svg:marker")
+      .attr("id", String)
+      .attr("viewBox", "0 -5 10 10")
+      .attr("refX", 23)
+      .attr("markerWidth", 6)
+      .attr("markerHeight", 6)
+      .attr("orient", "auto")
+      .append("svg:path")
+      .attr("d", "M0,-4L10,0L0,4Z");
 
-    var link = svg.selectAll(".link")
-        .data(json.links)
-      .enter().append("line")
-        .attr("class", "link")
-        .style("stroke", function(d) {
-          if (d.value == 1) { return "#333"; }
-          // return d.value;
-        })
-        // .style("stroke-width", function(d) {
-        //   if (d.value == 1) {
-        //     return "2";
-        //   } else { return 1; }
-        // })
-        .attr("marker-end", "url(#arrow)");
+  var link = svg.selectAll(".link")
+      .data(json.links)
+    .enter().append("line")
+      .attr("class", "link")
+      .style("stroke", function(d) {
+        if (d.value == 1) { return "#333"; }
+      })
+      .attr("marker-end", "url(#arrow)");
 
-    var node = svg.selectAll(".node")
-        .data(json.nodes)
-      .enter().append("g")
-        .attr("class", "node")
-        .call(force.drag);
+  var node = svg.selectAll(".node")
+      .data(json.nodes)
+    .enter().append("g")
+      .attr("class", "node")
+      .call(force.drag);
 
-    node.append("circle")
-        // .attr("r", 8)
-        .attr("r", function(d) {
-          if (d.group == "path") {
-            return 10;
-          } else { return 8; }
-        })
-        // .style("stroke", function(d) {
-        //   if (d.group == "path") {
-        //     return "#333";
-        //   } else { return "#fff"; }
-        // })
-        // .style("opacity", 0.8)
-        .style("fill", function(d) {
-          if (d.group == "path") {
-            return "#333";
-          } else { return color(d.type); }
-        });
-        // .style("fill", function(d) { return color(d.type); });
+  node.append("circle")
+      .attr("r", function(d) {
+        if (d.group == "path") {
+          return 10;
+        } else { return 8; }
+      })
+      .style("fill", function(d) {
+        if (d.group == "path") {
+          return "#333";
+        } else { return color(d.type); }
+      });
 
     // node.append("text")
     //     .attr("dx", 23)
     //     .attr("dy", ".35em")
-    //     // .style("fill", "white")
     //     .text(function(d) { return d.name; });
 
-    node.append("title")
-        .text(function(d) { return d.name + " (" + d.id + "), " + d.type; });
+  node.append("title")
+      .text(function(d) { return d.name + " (" + d.id + "), " + d.type; });
 
-    force.on("tick", function() {
-      link.attr("x1", function(d) { return d.source.x; })
-          .attr("y1", function(d) { return d.source.y; })
-          .attr("x2", function(d) { return d.target.x; })
-          .attr("y2", function(d) { return d.target.y; });
+  force.on("tick", function() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
 
-      node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-    });
-  // });
+    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+  });
 }
