25c6bffba7bde7b0832857118eac7a5c7982caae
erabug <erabug@gmail.com>
2014-11-17 17:37:30 -0800

    organized node codes and titles into an object, still working on identifying start and end nodes

--- static/index.js
+++ static/index.js
@@ -1,12 +1,12 @@
-var node1Code;
-var node2Code;
-var node1Title;
-var node2Title;
 var response;
+
+c = { 'node1': { 'code': '', 'title': '' },
+      'node2': { 'code': '', 'title': '' } };
 
 function clear_all() {
     $('input#start-node').val('');
     $('input#end-node').val('');
+    $('.path').val('');
     $('svg').remove();
 }
 
@@ -21,8 +21,10 @@ $('input#submit-query').click(function(e) {
     clear_all();
 
     var URL = 'http://en.wikipedia.org/w/api.php';
-    var queryParams = '?action=query&format=json&redirects&prop=pageimages&pithumbsize=100px&pilimit=2';
-    var pagesParams = node1Title + '|' + node2Title;
+    var queryParams = '?action=query&format=json&redirects&'+
+        'prop=pageimages&pithumbsize=100px&pilimit=2';
+    var pagesParams = c['node1']['title'] + '|' + c['node2']['title'];
+
     console.log(pagesParams);
 
     $.getJSON(
@@ -32,25 +34,41 @@ $('input#submit-query').click(function(e) {
             console.log(data);
             var pageObject = data['query']['pages'];
 
-            Object.keys(pageObject).forEach(function(pageKey, i) {
+            var startNode;
+            var endNode;
+
+            Object.keys(pageObject).forEach(function(pageKey) {
+                var node;
                 var page = pageObject[pageKey];
                 var title = page['title'];
                 var thumbnail = page['thumbnail']['source'];
-                $('.path').append('<div class="page"><div class="squareimg"><img id=' + i.toString() + ' src=' + thumbnail + '></div>'+'<div class="subtitle">'+title+'</div>'+'</div');
+                $('.path').append('<div class="page"><div class="squareimg">'+
+                    '<img src='+thumbnail+'></div>'+
+                    '<div class="subtitle">'+title+'</div></div>');
+                //iterate through values of keys
+                Object.keys(c).forEach(function(node) {
+                    if (c[node]['title'] == title) {
+                        console.log("match!");
+                    }
+                });
+
+                // startNode = '<div class="page"><div class="squareimg">'+
+                //     '<img src='+thumbnail+'></div>'+
+                //     '<div class="subtitle">'+title+'</div></div>';
+
             });
                 
-
         });
 
 	$.get(
 		'/query',
-		{'node1': node1Code.toString(), 'node2': node2Code.toString()},
+		{'node1': c['node1']['code'], 'node2': c['node2']['code']},
 		function(data) {
 
 			response = JSON.parse(data);
 			drawGraph(response);
             
-
+            // insert floating heads for the inner path nodes here
             
 
 		});
@@ -88,13 +106,17 @@ $('.scrollable-dropdown-menu .typeahead').typeahead(null, {
     source: pageNames.ttAdapter()
 });
 
+function decodeInput(d, node) {
+    c[node]['code'] = d.code.toString();
+    c[node]['title'] = d.value;
+    return null;
+}
+
 // records the values chosen for each field as a global var
 $('#start-node').on('typeahead:selected', function (e, d) {
-    node1Code = d.code;
-    node1Title = d.value;
+    decodeInput(d, 'node1');
 });
 
 $('#end-node').on('typeahead:selected', function (e, d) {
-    node2Code = d.code;
-    node2Title = d.value;
+    decodeInput(d, 'node2');
 });
