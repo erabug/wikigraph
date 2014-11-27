12e08930eb3ddfb24cf7c2d56fc3d979414cb59b
erabug <erabug@gmail.com>
2014-11-26 17:52:54 -0800

    added function to prune pages that have no outgoing links

--- master_clean.py
+++ master_clean.py
@@ -21,24 +21,34 @@ def convert_to_unicode(title):
 
 def assemble_dict(link_path, redirects):
     """Iterates through the pagelinks file and returns a dictionary containing
-    information about the page, its unique code, and what it links to.
-    Dictionary looks like: 
-    {'page1': {'code': 41, 'title': 'page1', 'links': set([42, 108])}}"""
+    information about the page, its unique code, and what it links to (if 
+    anything).
+
+    Example of returned dictionary: 
+    {'page1': {'code': 41, 
+               'title': 'page1', 
+               'links': set([42, 108])}}"""
 
     with open(link_path, 'rb') as paths:
         data = {}
-        # counter = 5000
+        counter = 50000
         foo = 0
-        id_counter = 0
+        code_counter = 0
 
         t0 = time.time()
         for line in paths:
-            # if counter > 0:
+            if counter > 0:
                 l = line.split('\t')
                 start = l[0]
                 end = l[1].rstrip()
 
-                if end[:5] == "File:" or end[:9] == "Category:" or start in redirects:
+                if end[:5] == "File:" or end[:9] == "Category:":
+                    continue
+
+                if start in redirects or start == end:
+                    continue
+
+                if end[:2] == "S:" or end[:4] == "Help:":
                     continue
 
                 if '%' in start:
@@ -51,32 +61,86 @@ def assemble_dict(link_path, redirects):
                     end = redirects[end] # replace it with the real page
 
                 if start not in data: 
-                    sid = id_counter # assign the page a code
-                    id_counter += 1
+                    sid = code_counter # assign the page a code
+                    code_counter += 1
 
                 if end not in data:
-                    eid = id_counter # assign the page a code
-                    id_counter += 1
+                    eid = code_counter # assign the page a code
+                    code_counter += 1
                 else:
                     eid = data[end]['code'] # else look up its code in data
 
                 if data.get(start, 0) == 0:
-                    data[start] = {'code': sid, 'title': start, 'links': {eid}}
+                    data[start] = {'code': sid, 
+                                   'title': start, 
+                                   'links': {eid}}
                 else:
                     data[start]['links'].add(eid)
 
                 if data.get(end, 0) == 0:
-                    data[end] = {'code': eid, 'title': end, 'links': set()}
+                    data[end] = {'code': eid, 
+                                 'title': end, 
+                                 'links': set()}
 
                 foo += 1
                 if foo % 10000000 == 0:
-                    time_elapsed = (time.time() - t0)/60
-                    print """%d million lines read in %.2f minutes""" % (foo/1000000, time_elapsed)
+                    x = foo/1000000
+                    y = (time.time() - t0)/60
+                    print "%d million lines read in %.2f minutes" % (x, y)
   
-            #     counter -= 1
-            # else: 
-            #     break
+                counter -= 1
+            else: 
+                break
+
     return data
+
+def recode_dict(data):
+    """Returns a dictionary of codes with pages as keys. If the page has 
+    outgoing links, its value is a new code; otherwise, its value is None.
+
+    Example of returned dictionary:
+    {{41: None}, {23: None}, {45: 2}}"""
+
+    codes = {}
+
+
+    # iterate through data, give new code to pages that have outgoing links
+    code_counter = 0
+    for value in data.values():
+        if value['links']:
+            codes[value['code']] = code_counter
+            code_counter += 1
+        else:
+            codes[value['code']] = None
+    return codes
+
+def prune_data(data, codes):
+    """Returns a dictionary of page links with continuous, sequential codes
+    that have at least one outgoing link. Also adds a field for degrees.
+
+    Example of returned dictionary:
+    {'page1': {'code': 41, 
+               'title': 'page1', 
+               'degrees': 2, 
+               'links': set([42, 108])}}"""
+    
+    # add information to new dict for pages with outgoing links    
+    pruned_data = {}
+    for i in data.keys():
+        value = data.pop(i)
+        if value['links']:
+            new_links = set()
+            for link in value['links']: # iterate through the set of links
+                if codes.get(link) != None:
+                    new_links.add(codes[link])
+            if new_links:
+                title = value['title']
+                pruned_data[title] = {'code': codes[value['code']],
+                                       'title': title,
+                                       'degrees': len(new_links),
+                                       'links': new_links}
+
+    return pruned_data
 
 def write_rels(data, rels_path):
     """Iterates through the information dictionary and writes the results to 
@@ -86,11 +150,8 @@ def write_rels(data, rels_path):
         rels.write('start\tend\ttype\n') # write headers
         # grab each key, iterate through its values to write lines to rels.tsv
         for key, value in data.iteritems():
-            link_counter = 0
             for lk in value['links']:
                 rels.write(str(value['code']) + '\t' + str(lk) + '\tLINKS_TO\n')
-                link_counter += 1
-            data[key].update({'degrees': link_counter}) # add degrees here!
 
 def write_nodes(data, nodes_path):
     """Iterates through the information dictionary (sorted by code number)
@@ -115,10 +176,16 @@ def clean_data():
     redirects = redirects_dict('data/cleaned_redirects.tsv')
     print "Reading page links..."
     data = assemble_dict('data/cleaned_links.tsv', redirects)
+    print "Page links dictionary created (length: %d)" % len(data)
+    print "Creating dictionary of new codes..."
+    codes = recode_dict(data)
+    print "Pruning data..."
+    pruned_data = prune_data(data, codes)
+    print "Page links dictionary pruned (length: %d)" % len(pruned_data)
     print "Writing 'rels.tsv'..."
-    write_rels(data, 'data/rels.tsv')
+    write_rels(pruned_data, 'data/rels.tsv')
     print "Writing 'nodes.tsv'..."
-    write_nodes(data, 'data/nodes.tsv')
+    write_nodes(pruned_data, 'data/nodes.tsv')
 
 if __name__ == "__main__":
     clean_data()
