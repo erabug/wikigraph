a3c70abbd453c0a20ebf7a972207edebabb7bdab
erabug <erabug@gmail.com>
2014-11-30 19:30:14 -0800

    updated db to include degrees column

--- create_pagenames_db.py
+++ create_pagenames_db.py
@@ -2,15 +2,20 @@ import csv, sqlite3
 
 conn = sqlite3.connect("pagenames.db")
 curs = conn.cursor()
-curs.execute("CREATE TABLE pagenames (code INTEGER PRIMARY KEY, title TEXT);")
+curs.execute("CREATE TABLE pagenames (code INTEGER PRIMARY KEY, title TEXT, degrees INTEGER);")
 
 with open('data/nodes.tsv', 'rb') as csv_file:
-    sql_insert = 'INSERT INTO pagenames VALUES(?, ?)'
+    sql_insert = 'INSERT INTO pagenames VALUES(?, ?, ?)'
     reader = csv.reader(csv_file, delimiter='\t')
     reader.next()
     for row in reader:
-    	to_db = [int(row[0]), unicode(row[1].replace('_', ' '), 'utf8')]
+        to_db = [int(row[0]), unicode(row[1].replace('_', ' '), 'utf8'), int(row[3])]
         curs.execute(sql_insert, to_db)
 
 conn.commit()
+
+curs.execute("CREATE UNIQUE INDEX codes ON pagenames(code);")
+curs.execute("CREATE INDEX titles ON pagenames(title);")
+curs.execute("CREATE INDEX degrees ON pagenames(degrees);")
+
 conn.close()
